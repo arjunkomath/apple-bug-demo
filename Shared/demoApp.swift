cae123c36fbcfa5f2d9a35f9ab17602ff78d0058
Arjun Komath <arjun@hey.com>
2021-08-19 19:53:26 +1000

    Initial Commit

---
+++ Shared/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  Shared
+//
+//  Created by Arjun Komath on 19/8/21.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
